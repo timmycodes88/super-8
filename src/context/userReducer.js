26a336bf0f10636e6b0774b72cb505f109ac3914
Tim <tim@brightsidedeveloper.com>
2022-11-05 15:32:50 -0500

    Added Reducer

--- src/context/userReducer.js
+++ src/context/userReducer.js
@@ -3,7 +3,8 @@ import { SET_PROFILE } from "./types";
 export default function userReducer(state, action) {
   switch (action.type) {
     case SET_PROFILE:
-      return state;
+      const newProfile = state.profile; //IN PROGRESS
+      return { ...state, profile: { ...state.profile, newProfile } };
     default:
       alert("REDUCER ERROR");
       return state;
